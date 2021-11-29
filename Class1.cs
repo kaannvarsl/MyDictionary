ddb2b3af7f9351cd6a7633de0e905526fa163e68
Pc1 <Pc1@DESKTOP-DSVIJLO>
2021-11-29 15:42:18 +0300

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[]args)
+        {
+            MyDictionary<int,string> myDictionary=new MyDictionary<int,string>();
+            myDictionary.Add(5600,"Kaan");
+            myDictionary.Add(2800, "Salih");
+            Console.WriteLine("Ankara");
+  
+        }
+    }
+
+
+
+}    
---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace MyDictionary
+{
+    class MyDictionary<T,J>
+    {
+        T[] keys;
+        J[] values;
+        T[] tempKey;
+        J[] tempValue;
+
+        public MyDictionary()
+        {
+            keys = new T[0];
+            values = new J[0];
+
+        }
+         public void Add(T key, J value)
+        {
+            tempKey = keys;
+            tempValue = values;
+            keys = new T[keys.Length + 1];
+            values = new J[values.Length + 1];
+
+            for (int i = 0 ;  i< tempKey.Length; i++)
+            {
+                keys[i]= tempKey[i];
+                values[i] = tempValue[i];
+            }
+             keys[keys.Length-1] = key;
+            values[keys.Length - 1] = value;
+            Console.WriteLine("Eklendi");
+        }
+        
+     }
+}    
+   
